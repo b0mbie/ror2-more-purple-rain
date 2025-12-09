141605d9b1849c4174e2e25ce1ba69b06bd15a78
b0mbie <69766525+b0mbie@users.noreply.github.com>
2025-12-09 22:32:11 +0200

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using RoR2;
+
+namespace more_purple_rain;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin {
+	internal static new ManualLogSource Logger;
+
+	const string PURPLE_RAIN_SONG_NAME = "muSong14";
+
+	private void Awake() {
+		// Plugin startup logic
+		Logger = base.Logger;
+		Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+		MusicController.pickTrackHook += (MusicController controller, ref MusicTrackDef newTrack) => {
+			var mainTrack = SceneCatalog.mostRecentSceneDef.mainTrack;
+			if (mainTrack.cachedName == PURPLE_RAIN_SONG_NAME && newTrack.cachedName != PURPLE_RAIN_SONG_NAME) {
+				Logger.LogDebug("Continuing Sky Meadows main track...");
+				newTrack = mainTrack;
+			}
+		};
+	}
+}
